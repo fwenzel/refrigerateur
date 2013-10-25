dd375bd0c0dc01208c68a071288ceb5f2b23a00e
Fred Wenzel <fwenzel@mozilla.com>
2013-10-24 21:07:29 -0700

    Tabs, really?

--- data/js/refrigerateur.js
+++ data/js/refrigerateur.js
@@ -1,20 +1,20 @@
 (function() {
 
 /**
- * Collect GIFs from chat.meatspac.es
- */
+* Collect GIFs from chat.meatspac.es
+*/
 function collectMeat() {
-	// Messages that haven't been scraped yet.
-	var lis = document.querySelectorAll('.chats li[data-key]:not(.scraped)');
-	for (var li of lis) {
-		li.className += ' scraped';
-		var key = li.getAttribute('data-key');
+    // Messages that haven't been scraped yet.
+    var lis = document.querySelectorAll('.chats li[data-key]:not(.scraped)');
+    for (var li of lis) {
+        li.className += ' scraped';
+        var key = li.getAttribute('data-key');
+        var src = li.querySelector('img').src;
 
-		var src = li.querySelector('img').src;
-		// Extract data from data URI
-		var gif = atob(src.substr(src.indexOf(',') + 1));
-		self.port.emit('foundMeat', [key, gif]);
-	}
+        // Extract data from data URI
+        var gif = atob(src.substr(src.indexOf(',') + 1));
+        self.port.emit('foundMeat', [key, gif]);
+    }
 }
 
 // Collect new images every few seconds.
